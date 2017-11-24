30267b717eda9be52489faa9a77015ca460b9df9
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-25 10:09:34 +1100

    First commit added class with methods

---
+++ pass_by_value_and_reference/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pass_by_value_and_reference
+{
+    class MethodDemo
+    {
+        public void PassByValue(int a)
+        {
+            a = 10;
+            Console.WriteLine("a inside method = {0}", a);
+        }
+
+        public void
+        PassByReference(int[] b)
+        {
+            b[0] = 5;
+            Console.WriteLine("b[0] inside method = {0}", b[0]);
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
